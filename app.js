f0bdefcf04868656f1859becdcac67833d45daac
PANDYCH <117732133+Pandych007@users.noreply.github.com>
2024-12-21 12:55:19 +0300

    first commit

--- app.js
+++ app.js
@@ -11,3 +11,45 @@ htmlSections.forEach((section) => {
     }
   });
 });
+// ________________________________________________________________
+
+let myMrogekt = document.getElementById("myMrogekt");
+
+myMrogekt.addEventListener("click", function () {
+  document.getElementById("contentModalAbout").style.display = "none";
+  document.getElementById("contentModalContakt").style.display = "none";
+
+  if (document.getElementById("contentModal").style.display == "block") {
+    document.getElementById("contentModal").style.display = "none";
+  } else {
+    document.getElementById("contentModal").style.display = "block";
+  }
+});
+// ________________________________________________________________
+let about = document.getElementById("about");
+
+about.addEventListener("click", function () {
+  document.getElementById("contentModal").style.display = "none";
+  document.getElementById("contentModalContakt").style.display = "none";
+
+  if (document.getElementById("contentModalAbout").style.display == "block") {
+    document.getElementById("contentModalAbout").style.display = "none";
+  } else {
+    document.getElementById("contentModalAbout").style.display = "block";
+  }
+});
+
+// ________________________________________________________________
+
+let contact = document.getElementById("contact");
+
+contact.addEventListener("click", function () {
+  document.getElementById("contentModal").style.display = "none";
+  document.getElementById("contentModalAbout").style.display = "none";
+  if (document.getElementById("contentModalContakt").style.display == "block") {
+    document.getElementById("contentModalContakt").style.display = "none";
+  } else {
+    document.getElementById("contentModalContakt").style.display = "block";
+  }
+});
+//  ________________________________________________________________
