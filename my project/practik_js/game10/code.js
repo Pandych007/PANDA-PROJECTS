f1d4002126eb173fdcca705b7ddbdeade41b7e7c
PANDYCH <117732133+Pandych007@users.noreply.github.com>
2025-01-18 13:07:47 +0300

    first commit

--- my project/practik_js/game10/code.js
+++ my project/practik_js/game10/code.js
@@ -1,12 +1,22 @@
+// время легкий уровень 7 мин, надо набрать очков 20, кол во попыток 20, диапазон от 1 до 20, кол во подсказок 2, одно угаданное число дает 5 очков,  надо угадать 5 чисел
+
+// время средний  уровень 6 мин,  надо набрать  24, кол во попыток 15, диапазон от 1 до 30, кол во подсказок 3, одно угаданное число дает 4 очка, надо угадать 6 чисел
+
+// время сложный уровень 6 мин,  надо набрать 27 , кол во попыток 15, диапазон от 1 до 40, кол во подсказок 5, одно угаданное число дает 3 очка, надо угадать 9 чисел
+
+// время хард кор уровень 5 мин, надо набрать  10, кол во попыток 10, диапазон от 1 до 60, кол во подсказок 7, одно угаданное число дает 1 очко, надо угадать 10 чисел
+
 // let point = 0;
-// let count = 20;
+//
 // let nRm = 0;
 // point
-let limitiLev2 = 10;
-let limitiLev3 = 20;
-let limitiLev4 = 25;
-let limitLev5 = 15;
-let gameOver = 70; // за все уровни
+let limitPoint = 20;
+let point = 0;
+let diapazon = 20;
+let countPoputki = 20;
+let cluePodskaska = 2;
+
+// за все уровни
 
 let nuwRandomNumber;
 // let diapazonL1 = 20;
@@ -14,11 +24,11 @@ let nuwRandomNumber;
 // let diapazonL3 = 40;
 // let diapazonL4 = 60;
 // timer
-let timerl1 = 6;
+let timerl1 = 7;
 let timerl1s = "00";
-let timerl2 = 3;
+let timerl2 = 6;
 let timerl2s = "00";
-let timerl3 = 4;
+let timerl3 = 6;
 let timerl3s = "00";
 let timerl4 = 5;
 let timerl4s = "00";
@@ -27,7 +37,7 @@ let timer = 0;
 let timerS = "00";
 let textLavel = "";
 let carenLavcel = 0;
-
+let interval;
 // let proverka = document.getElementById("proverka");
 // let numberUser;
 // document.querySelector(".count").textContent = `колличество попвток ${count}`;
@@ -72,13 +82,13 @@ levelChange.forEach((elem, index) => {
     if (lavelText == "Легкий уровень") {
       carenLavcel = 1;
     } else if (lavelText == "Средний уровень") {
-      if (point >= limitiLev2) carenLavcel = 2;
+      if (point >= 20) carenLavcel = 2;
       else alert("Сначала пройди легкий  уровень");
     } else if (lavelText == "Сложный уровень") {
-      if (point >= limitiLev3) carenLavcel = 3;
+      if (point >= 24) carenLavcel = 3;
       else alert("Сначала пройди средний  уровень");
     } else if (lavelText == "хард кор") {
-      if (point >= limitiLev4) carenLavcel = 4;
+      if (point >= 27) carenLavcel = 4;
       else alert("Сначала пройди сложный  уровень");
     }
     if (carenLavcel != 0) {
@@ -91,11 +101,43 @@ levelChange.forEach((elem, index) => {
         timer = timerl1;
         timerS = timerl1s;
         textLavel = "Легкий уровень";
+        limitPoint = 20;
+        diapazon = 20;
+        countPoputki = 20;
+        cluePodskaska = 2;
+        break;
+      case 2:
+        timer = timerl2;
+        timerS = timerl2s;
+        textLavel = "Средний уровень";
+        limitPoint = 24;
+        diapazon = 40;
+        countPoputki = 15;
+        cluePodskaska = 3;
+        break;
+      case 3:
+        timer = timerl3;
+        timerS = timerl3s;
+        textLavel = "Сложный уровень";
+        limitPoint = 27;
+        diapazon = 20;
+        countPoputki = 15;
+        cluePodskaska = 5;
+        break;
+      case 4:
+        timer = timerl4;
+        timerS = timerl4s;
+        textLavel = "хард кор уровень";
+        limitPoint = 10;
+        diapazon = 60;
+        countPoputki = 10;
+        cluePodskaska = 7;
         break;
     }
     document.querySelector(".lavel1").textContent = textLavel;
     document.querySelector(".timer1").textContent =
       timerl1.toString() + ":" + timerl1s.toString();
+    document.getElementById("point1").textContent = point.toString();
   });
 });
 
@@ -136,9 +178,10 @@ timer1.addEventListener("click", function () {
   nuwRandomNumber = randomNamber(minDiapazon, maxDiapazon);
 
   startTimer = true;
-  const interval = setInterval(() => {
+  interval = setInterval(() => {
     const minutes = Math.floor(totalTime / 60);
     const seconds = totalTime % 60;
+    console.log(totalTime);
     totalTime--;
     timer1.textContent = `${minutes.toString().padStart(2, "0")} : ${seconds
       .toString()
@@ -162,16 +205,36 @@ startGameBTN.addEventListener("click", function () {
     } else {
       if (numberInput == nuwRandomNumber) {
         alert("вы угадали чило");
-        count = 20;
-        nRm = 0;
-        point++;
+        if (carenLavcel == 1) {
+          point += 5;
+          countPoputki--;
+          if (point == 20) {
+            carenLavcel = 2;
+            // document.querySelector(".lavel1").textContent ="Средний уровень"
+          }
+        } else if (carenLavcel == 2) {
+          point += 4;
+          countPoputki--;
+        } else if (carenLavcel == 3) {
+          point += 3;
+          countPoputki--;
+        } else if (carenLavcel == 4) {
+          point += 1;
+          countPoputki--;
+        }
+        document.getElementById("point1").textContent = point;
+        document.getElementById("countPoputki").textContent = countPoputki;
+        clearInterval(interval);
+        // count = 20;
+        // nRm = 0;
+        // point++;
         document.body.style.backgroundColor = "green";
       } else if (nuwRandomNumber > numberInput) {
         alert("число  больше");
-        count--;
+        countPoputki--;
       } else if (nuwRandomNumber < numberInput) {
         alert("число меньше");
-        count--;
+        countPoputki--;
       }
     }
   }
@@ -181,3 +244,4 @@ let exsitGame = document.getElementById("exsitGame");
 exsitGame.addEventListener("click", function () {
   window.location.replace("index.html");
 });
+// _________________________________________________________________
